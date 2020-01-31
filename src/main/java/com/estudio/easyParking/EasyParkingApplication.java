d37308aa9a72f73faa1e4ea41137f1aa047db5d3
kolarte <kolarte@unab.edu.co>
2020-01-31 02:58:54 -0500

    Commit de inicio
    es el commit del proyecto sin modificaciones

---
+++ src/main/java/com/estudio/easyParking/EasyParkingApplication.java
@@ -0,0 +1,13 @@
+package com.estudio.easyParking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudio/easyParking/EasyParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudio.easyParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
